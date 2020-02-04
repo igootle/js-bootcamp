3a10b55f7c260f87d34d030828c51a6d14eb9f4a
Rapeepat Sangsin <rapeepatsangsin@igootle.local>
2020-02-04 14:48:14 +0700

    variable  commit

---
+++ basics/temp-conversion.js
@@ -0,0 +1,7 @@
+let fahrenheit = 32 // 0c - 273.15k
+
+let celcius = (fahrenheit-32) * (5/9)
+let kalvin = (fahrenheit+459.67) * (5/9)
+
+console.log(celcius)
+console.log(kalvin)
---
+++ basics/variables.js
@@ -0,0 +1,12 @@
+let petName = 'Hal'
+petName = 'Spot'
+
+console.log(petName)
+
+// 2. There are rules related to variable names
+let test_ = 2
+let result = 3 + 4
+
+// 3. Variable names cannot be reserved keywords
+// let let = 12
+
